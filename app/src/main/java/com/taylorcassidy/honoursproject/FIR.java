65ed660fa2afb708909f4235fb25695eecb62f66
TaylorJCassidy <taylorjcassidy1@gmail.com>
2023-12-11 11:00:41 +0000

    add FIR filter, print filtered values to screen

---
+++ app/src/main/java/com/taylorcassidy/honoursproject/FIR.java
@@ -0,0 +1,41 @@
+package com.taylorcassidy.honoursproject;
+
+public class FIR {
+
+    private final float[] buffer;
+    private final int bufferSize;
+    private final float[] coefficients;
+    private int bufferPosition = 0;
+
+    public FIR(float[] coefficients) {
+        bufferSize = coefficients.length;
+        buffer = new float[bufferSize];
+        this.coefficients = coefficients;
+    }
+
+    public FIR(int coefficientCount) {
+        this(generateCoefficients(coefficientCount));
+    }
+    
+    public float filter(float value) {
+        buffer[bufferPosition] = value;
+
+        float result = 0f;
+        for (int i = 0; i < bufferSize; i++) {
+            int index = Math.floorMod(bufferPosition - i, bufferSize);
+            result += coefficients[i] * buffer[index];
+        }
+
+        if (++bufferPosition >= bufferSize) bufferPosition = 0;
+
+        return result;
+    }
+
+    private static float[] generateCoefficients(int coefficientCount) {
+        float[] coefficients = new float[coefficientCount];
+        for (int i = 0; i < coefficientCount; i++) {
+            coefficients[i] = 1f / (coefficientCount);
+        }
+        return coefficients;
+    }
+}
--- app/src/main/java/com/taylorcassidy/honoursproject/MainActivity.java
+++ app/src/main/java/com/taylorcassidy/honoursproject/MainActivity.java
@@ -2,13 +2,52 @@ package com.taylorcassidy.honoursproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
+import com.taylorcassidy.honoursproject.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        FIR xFir = new FIR(5);
+        FIR yFir = new FIR(5);
+        FIR zFir = new FIR(5);
+
+        SensorEventListener sensorEventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float[] values = {
+                        xFir.filter(event.values[0]),
+                        yFir.filter(event.values[1]),
+                        zFir.filter(event.values[2])
+                };
+                binding.X.setText(String.valueOf(values[0]));
+                binding.Y.setText(String.valueOf(values[1]));
+                binding.Z.setText(String.valueOf(values[2]));
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                System.out.println(sensor.getName());
+            }
+        };
+
+        sensorManager.registerListener(sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
     }
+
 }
